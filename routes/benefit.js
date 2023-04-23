3a53540b43971e6637f3c463beb392e50c7fd6d7
Mirjahon <mirjahonqayimov@gmail.com>
2023-04-23 13:13:00 +0500

    some changes

--- routes/benefit.js
+++ routes/benefit.js
@@ -1,5 +1,10 @@
 router.get('/get/benefit',checker,async(req,res)=>{
+    let {kurs}=await RunSQLOne("select kurs from valuta")
+    console.log(kurs)
     let data=[]
+    const {uzsdata}=await RunSQLOne("select sum((sell_price-price)*pr_count) as uzsdata from products p join filial_count f on f.product_id=p.product_id where p.ISSUM=1")
+    const {usddata}=await RunSQLOne("select sum((sell_price-price)*pr_count) as usddata from products p join filial_count f on f.product_id=p.product_id where p.ISSUM=0")
+    console.log(uzsdata)
     if (req.query.month)
     data=await RunSQL("select sum(pr_count*(sellprice-price)) as summa,cr_date from tranzactions where year(cr_date)=? and  month(cr_date)=? group by DAte(cr_date)",[parseInt(req.query.month.split('-')[0]),parseInt(req.query.month.split('-')[1])]);
     else if (req.query.year){
@@ -7,6 +12,9 @@ router.get('/get/benefit',checker,async(req,res)=>{
     }
     res.render('benefit',{
         data:data,
+        uzsdata:uzsdata,
+        usddata:usddata,
+        allsumma:parseInt(uzsdata)+parseInt(usddata*kurs),
         count:31 
        })
 })
